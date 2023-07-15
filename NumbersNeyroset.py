8b3f04a0a0b729eec87b01d0ab17059623aa02c8
AzyWorld <dimka15082008@mail.ru>
2023-07-15 19:50:35 +0500

    New Drawing system and models

--- NumbersNeyroset.py
+++ NumbersNeyroset.py
@@ -5,24 +5,40 @@ pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
 sur = my_font.render("1", (0,0,0), (0,0,0))
+texts = [my_font.render("1", (0,0,0), (0,0,0)), my_font.render("1", (0,0,0), (0,0,0)),my_font.render("1", (0,0,0), (0,0,0)), my_font.render("1", (0,0,0), (0,0,0)),my_font.render("1", (0,0,0), (0,0,0)), my_font.render("1", (0,0,0), (0,0,0)),my_font.render("1", (0,0,0), (0,0,0)), my_font.render("1", (0,0,0), (0,0,0)),my_font.render("1", (0,0,0), (0,0,0)), my_font.render("1", (0,0,0), (0,0,0))]
 
 class pixel:
     def __init__(self, x, y):
         self.x = x*20+5
         self.y = y*20+7.5
-        self.color = (0, 0, 0)
+        self.r = 0
+        self.g = 0
+        self.b = 0
+        self.color = (self.r, self.g, self.b)
     def paint(self, pos):
         if pos[0] <= self.x+20 and pos[0] >= self.x and pos[1] >= self.y and pos[1] <= self.y+20:
-            if IsMouseBut[1] == 1:
-                self.color = (230, 230, 230)
-            else:
+            if IsMouseBut[1] == 1 and self.color[0] < 240:
+                self.r += 20
+                self.g += 20
+                self.b += 20
+            elif IsMouseBut[1] == 3:
                 self.color = (0, 0, 0)
+                self.r = 0
+                self.b = 0
+                self.g = 0
         elif pos[0] <= self.x+40 and pos[0] >= self.x-20 and pos[1] >= self.y-20 and pos[1] <= self.y+40:
-            if IsMouseBut[1] == 1 and self.color != (230, 230, 230):
-                self.color = (127, 127, 127)
+            if IsMouseBut[1] == 1 and self.color[0] < 240:
+                self.r += 10
+                self.g += 10
+                self.b += 10
+            elif IsMouseBut[1] == 3:
+                self.color = (0, 0, 0)
+                self.r = 0
+                self.b = 0
+                self.g = 0
 
 
-WIDTH = 750
+WIDTH = 810
 HEIGHT = 570
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -60,15 +76,21 @@ while Processing:
             if event.key == pygame.K_BACKSPACE:
                 for i in pixels:
                     i.color = (0,0,0)
+                    i.r = 0
+                    i.g = 0
+                    i.b = 0
     for i in pixels:
         if IsMouseBut[0]:
             i.paint(event.pos)
+        i.color = (i.r, i.g, i.b)
         pygame.draw.rect(win, i.color, (i.x, i.y, 20, 20))
         img[int((i.y-7.5)/20), int((i.x-5)/20)] = i.color[0]/255
     x += 1
     if x > 300:
         x = 0
-        sur = my_font.render(str(neyroset.predict_img(img)), (255, 255, 255), (0, 0, 0))
-    win.blit(sur, (600, 10))
+        for i, prob in enumerate(neyroset.predict_img(img)[1]):
+            texts[i] = my_font.render(str(i) + " - " + str(prob*100)[:4] + "%", (255, 255, 255), (0, 0, 0))
+    for i in range(10):
+        win.blit(texts[i], (570, 10+50*i))
     clock.tick(60)
     pygame.display.update()
--- neyroset.py
+++ neyroset.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 import numpy as np
 
-model = tf.keras.models.load_model('my_model4.h5')   
+model = tf.keras.models.load_model('my_model8_(15_epochs_0.2_sm).keras')   
 
 def predict_img(img):
     processed_image = np.expand_dims(img, axis=0)
     predictions = model.predict(processed_image)
     predicted_class = tf.argmax(predictions[0]).numpy()
-    print(predicted_class)
-    return predicted_class
+    #for i, prob in enumerate(predictions[0]):
+        #print("Уверенность в цифре", i, ":", prob*100)
+    return [predicted_class, predictions[0]]
